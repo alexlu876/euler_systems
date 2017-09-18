92670ea10bac7bca35b397aa77b6475c1b606442
Alex Lu <Alex@Alexs-MBP-2.fios-router.home>
2017-09-18 01:16:51 -0400

    i did some stuff

--- foo.c
+++ foo.c
@@ -1,6 +1,41 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+int sum_square_diff(){
+  int a = (101 * 50) * (101 * 50);
+  int i;
+  for(i = 1; i < 101; i++){
+    a = a - (i ^ 2);
+  }
+  printf("The answer to Sum Square Difference is %d\n", a);
+  return a;
+}
+
+int even_fib(){
+  int a = 1;
+  int b = 2;
+  int sum = 2;
+  int i = 0;
+  while(a + b < 4000000){
+    if(i % 2 == 0){
+      a = a + b;
+      if(a % 2 == 0){
+	sum += a;
+      }
+    }
+    if(i % 2 == 1){
+      b = a + b;
+      if(b % 2 == 0){
+	sum += b;
+      }
+    }
+    i++;
+  }
+  printf("Sum of even fibs less than 4 million is %d\n", sum);
+  return sum;
+}
+    
+
 int mult_3_5(){
   int sum = 0;
   int i;
@@ -15,21 +50,7 @@ int mult_3_5(){
 
 int main(){
   mult_3_5();
+  even_fib();
+  sum_square_diff();
   return 0;
 }
-/*
-
-int even_fibs(){
-  int a = 1;
-  int b = 2;
-  int count = 0;
-  int sum = 2;
-  while(a + b < 4000000){
-    if(count % 2 = 0){
-      a = a + b;
-    }
-    if(count % 2 = 1){
-      b = a + b
-}
-  
-*/
